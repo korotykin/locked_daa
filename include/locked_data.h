3032df94d8c84b0d776959b3b6fd2c6f9a6f47c8
Alexander Korotykin <alexander.korotykin@gmail.com>
2019-11-09 18:48:59 +0400

    implementation

--- include/locked_data.h
+++ include/locked_data.h
@@ -1,1 +1,56 @@
 #pragma once
+
+#include <mutex>
+#include <type_traits>
+
+namespace akt
+{
+// predeclaration
+template <typename T>
+class LockedData;
+
+
+template <typename T>
+class LockableData
+{
+public:
+    LockedData<T> Lock() { return LockedData<T>(mMtx, mValue); }
+public:
+    template <typename... Args, typename std::enable_if<std::is_constructible<T, Args...>::value, int>::type = 0>
+    LockableData(Args&&... args): mValue(std::forward<Args>(args)...) {}
+/*
+    template <typename std::enable_if<std::is_default_constructible<T>::value && std::is_convertable<U, T>::value, int>::type = 0>
+    LockableData() {}
+    template <typename U = T, typename std::enable_if<std::is_copy_constructible<T>::value && std::is_convertable<U, T>::value, int>::type = 0>
+    LockableData(const U& rhv): mValue(rhv) {}
+    template <typename std::enable_if<std::is_move_constructible<T>::value, int>::type = 0>
+    LockableData(T&& rhv): mValue(std::forward<T>(rhv)) {}
+*/
+private:
+    T mValue;
+    std::mutex mMtx;
+};
+
+template <typename T>
+class LockedData
+{
+public:
+    LockedData(std::mutex& mtx, T& value): mGuard(mtx), mValue(value) {}
+
+public:
+    T& operator*() { return mValue; }
+    const T& operator*() const { return mValue; }
+    T* operator->() { return &mValue; }
+    const T* operator->() const { return &mValue; }
+public:
+    LockedData() = delete;
+    LockedData(const LockedData<T>&) = delete;
+    LockedData(LockedData<T>&& rhv): mGuard(std::move(rhv.mGuard)), mValue(rhv.mValue) {}
+    LockedData& operator=(const LockedData<T>&) = delete;
+    LockedData& operator=(LockedData<T>&& rhv) { mGuard = std::move(rhv.mGuard); mValue = rhv.mValue; }
+private:
+    std::unique_lock<std::mutex> mGuard;
+    T& mValue;
+};
+
+} // namespace akt
--- src/main.cpp
+++ src/main.cpp
@@ -3,3 +3,16 @@
 #include "locked_data.h"
 #include <catch2/catch.hpp>
 
+TEST_CASE("locked_data")
+{
+    akt::LockableData<int> a{1};
+    {
+        auto pa = a.Lock();
+        CHECK(1 == *pa);
+        *pa = 2;
+    }
+    {
+        auto pa = a.Lock();
+        CHECK(2 == *pa);
+    }
+}
